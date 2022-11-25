17479743d6954c798f1868918fcec32f03345839
KevinVeliz <mario-veliz258934@hotmail.com>
2022-11-25 18:18:49 -0500

    Página Home completa

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,7 +11,7 @@ const HomePage = () => {
                 <h1 className="text-home"> Welcome to my PlayGround</h1>
                 <div className="box-home">
                     <p className="text-home-p">
-                    Hi Everyone!, my name is Juan Sebastian Sotomayor 
+                    Hi Everyone!, my name is Kevin Orlando Veliz Montes,
                     and this is my playground, check some of my projects. If you like what you see don’t hesitate to contact me, I’ll be glad to help you
                     </p>
                 </div>
